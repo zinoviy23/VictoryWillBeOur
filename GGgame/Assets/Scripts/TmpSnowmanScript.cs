2d39a49ed142cdd41b5e9e2a8d23401d7752725f
zinoviy23 <zinoviy23@gmail.com>
2017-12-02 21:07:08 +0300

    Snegovig script

---
+++ GGgame/Assets/Scripts/TmpSnowmanScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TmpSnowmanScript : MonoBehaviour {
+
+    /// <summary>
+    /// Позиция игрока
+    /// </summary>
+    private Transform playersTransform;
+
+	// Use this for initialization
+	void Start () {
+        playersTransform = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(playersTransform);
+        transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
+	}
+}
