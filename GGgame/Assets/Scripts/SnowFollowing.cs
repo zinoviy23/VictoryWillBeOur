5d9792a308348895146c2cb69fe677687baba33a
zinoviy23 <zinoviy23@gmail.com>
2017-12-02 17:06:03 +0300

    Snow

---
+++ GGgame/Assets/Scripts/SnowFollowing.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowFollowing : MonoBehaviour {
+
+    /// <summary>
+    /// Позиция игрока
+    /// </summary>
+    private Transform playersTransform;
+    /// <summary>
+    /// Разница между игроком и снегом
+    /// </summary>
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        playersTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        offset = transform.position - playersTransform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = playersTransform.position + offset;
+	}
+}
