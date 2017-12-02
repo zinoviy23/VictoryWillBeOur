395ac01053c513a0658a9811918aec886419c03b
zinoviy23 <zinoviy23@gmail.com>
2017-12-02 20:55:08 +0300

    Dofiga vsego

---
+++ GGgame/Assets/Scripts/ArmScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmScript : MonoBehaviour {
+
+    /// <summary>
+    /// Префаб снежка
+    /// </summary>
+    public GameObject snowball; 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }	
+	}
+
+    /// <summary>
+    /// Метод для бросания шарика
+    /// </summary>
+    void Shoot()
+    {
+        Vector3 dir = Camera.main.ScreenPointToRay(
+                new Vector3(Screen.width / 2, Screen.height / 2)).direction;
+
+        GameObject obj = Instantiate(snowball, transform);
+        obj.transform.localPosition = Vector3.forward;
+
+        obj.GetComponent<Rigidbody>().AddForce(dir * 300);
+        obj.transform.parent = null;
+    }
+}
---
+++ GGgame/Assets/Scripts/SnowballScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowballScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(Die(5f));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    /// <summary>
+    /// Метод для уничтожения объекта
+    /// </summary>
+    /// <param name="dieTime"></param>
+    /// <returns></returns>
+    IEnumerator Die(float dieTime)
+    {
+        yield return new WaitForSeconds(dieTime);
+        Destroy(gameObject);
+    }
+}
