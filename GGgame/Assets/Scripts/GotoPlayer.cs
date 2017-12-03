1f864ae19f8152aa6bfa3b3d852358b51647a7f4
Makar Selivanov <makar.selivanov@gmail.com>
2017-12-04 02:18:29 +0300

    snowman can walk

---
+++ GGgame/Assets/Scripts/GotoPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class GotoPlayer : MonoBehaviour {
+    private NavMeshAgent agent;
+    public GameObject player;
+    // Use this for initialization
+    void Start () {
+        agent = GetComponent<NavMeshAgent>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        agent.destination = player.transform.position;
+    }
+}
