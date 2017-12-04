792417fcc746c8e21c97648c57278c528a39409c
Makar Selivanov <makar.selivanov@gmail.com>
2017-12-04 21:30:26 +0300

    All can move

---
+++ GGgame/Assets/Scripts/Target_Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.AI;
+
+public class Target_Player : MonoBehaviour {
+    private UnityEngine.AI.NavMeshAgent agent;
+    public GameObject player;
+    // Use this for initialization
+    void Start()
+    {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.destination = player.transform.position;
+    }
+}
